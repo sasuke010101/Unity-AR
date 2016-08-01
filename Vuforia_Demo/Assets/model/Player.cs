26ca01694036cbbc0b706429a470bf492b7d663d
Wu Haoxiang <WuHaoxiang@WudeMacBook-Pro.local>
2016-08-01 20:33:34 +0800

    add animation to it

---
+++ Vuforia_Demo/Assets/model/Player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Animation ani;
+	// Use this for initialization
+	void Start () {
+		ani = this.GetComponent<Animation> ();
+		ani.CrossFade ("idle");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
